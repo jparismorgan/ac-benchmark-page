2f66d1ad90d3552ace3c9f72adc79637b7c5f0e4
Paris Morgan <1396242+jparismorgan@users.noreply.github.com>
2022-03-24 13:28:54 -0700

    about to refactor buildEvents

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # ac-benchmark-page
+
 Created with CodeSandbox
+
+          // onMouseOver: () => {
+          //   return [
+          //     {
+          //       childName: ["area-" + idx],
+          //       target: "data",
+          //       eventKey: "all",
+          //       mutation: (props: any) => {
+          //         console.log(props.data[0]);
+          //         return {
+          //           style: { ...props.style, strokeWidth: 4, fillOpacity: 0.5 }
+          //         };
+          //       }
+          //     }
+          //   ];
+          // }
+          // onMouseOut: () => {
+          //   return [
+          //     {
+          //       childName: ["area-" + idx],
+          //       target: "data",
+          //       eventKey: "all",
+          //       mutation: () => {
+          //         return null;
+          //       }
+          //     }
+          //   ];
+          // }
--- src/App.tsx
+++ src/App.tsx
@@ -59,9 +59,9 @@ export default function App() {
   const [state, setState] = useState<State>({ hiddenSeries: new Set() });
   const [series, setSeries] = useState<Array<Line>>([
     {
-      sequence: "a",
+      sequence: "sequence-a",
       percentile: "q50",
-      name: "a-q50",
+      name: "sequence-a-q50",
       color: "pink",
       datapoints: [
         { x: 0, y: 5 },
@@ -70,9 +70,9 @@ export default function App() {
       ]
     },
     {
-      sequence: "a",
+      sequence: "sequence-a",
       percentile: "q99",
-      name: "a-q99",
+      name: "sequence-a-q99",
       color: "red",
       datapoints: [
         { x: 0, y: 6 },
@@ -81,9 +81,9 @@ export default function App() {
       ]
     },
     {
-      sequence: "b",
+      sequence: "sequence-b",
       percentile: "q50",
-      name: "b-q50",
+      name: "sequence-b-q50",
       color: "teal",
       datapoints: [
         { x: 0, y: 1 },
@@ -92,9 +92,9 @@ export default function App() {
       ]
     },
     {
-      sequence: "b",
+      sequence: "sequence-b",
       percentile: "q99",
-      name: "b-q99",
+      name: "sequence-b-q99",
       color: "blue",
       datapoints: [
         { x: 0, y: 2 },
@@ -105,6 +105,7 @@ export default function App() {
   ]);
 
   const percentiles: Array<string> = ["q50", "q99"];
+  const sequences: Array<string> = ["sequence-a", "sequence-b"];
 
   const buildEvents = () => {
     return percentiles.map((percentile, idx) => {
@@ -116,7 +117,7 @@ export default function App() {
           onClick: () => {
             return [
               {
-                childName: ["area-" + idx],
+                childName: "", // we don't mutate any elements directly with this, instead we modify hiddenSeries |  ["area-" + idx],
                 target: "data",
                 eventKey: "all",
                 mutation: () => {
@@ -124,7 +125,7 @@ export default function App() {
 
                   // We have the percentile and we want to show / hide all lines with that percentile
                   series.forEach((line) => {
-                    if (line.percentile === percentile) {
+                    if (line["percentile"] === percentile) {
                       if (!hiddenSeriesLocal.delete(line.name)) {
                         // Returns true if value was already in Set; otherwise false.
                         // Was not already hidden => add to set
@@ -132,14 +133,7 @@ export default function App() {
                       }
                     }
                   });
-
-                  // if (!state.hiddenSeries.delete(idx)) {
-                  //   // Returns true if value was already in Set; otherwise false.
-                  //   // Was not already hidden => add to set
-                  //   state.hiddenSeries.add(idx);
-                  // }
                   setState({
-                    // hiddenSeries: new Set(state.hiddenSeries)
                     hiddenSeries: new Set(hiddenSeriesLocal)
                   });
                   return null;
@@ -147,33 +141,6 @@ export default function App() {
               }
             ];
           }
-          // onMouseOver: () => {
-          //   return [
-          //     {
-          //       childName: ["area-" + idx],
-          //       target: "data",
-          //       eventKey: "all",
-          //       mutation: (props: any) => {
-          //         console.log(props.data[0]);
-          //         return {
-          //           style: { ...props.style, strokeWidth: 4, fillOpacity: 0.5 }
-          //         };
-          //       }
-          //     }
-          //   ];
-          // }
-          // onMouseOut: () => {
-          //   return [
-          //     {
-          //       childName: ["area-" + idx],
-          //       target: "data",
-          //       eventKey: "all",
-          //       mutation: () => {
-          //         return null;
-          //       }
-          //     }
-          //   ];
-          // }
         }
       };
     });
