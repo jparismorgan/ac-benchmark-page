d4a4ca2175aac58f5c2a25acb3fb4ff8fa5c574c
Paris Morgan <paris@8thwall.com>
2022-03-28 17:07:08 -0700

    Switch to use a single state variable

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ const percentiles: Array<string> = ["q99", "q50"];
 const sequences: Array<string> = ["seq-a", "seq-b"];
 
 const toVictoryLegend = (
-  hiddenKeys: Set<string>,
+  hiddenLegendKeys: Set<string>,
   lines: Array<Line>,
   key: string,
   colors: Array<string>
@@ -56,25 +56,32 @@ const toVictoryLegend = (
       name: p,
       symbol: {
         fill: colors[i % 2],
-        // type: hiddenKeys.has(p) ? "square" : "circle",
-        fillOpacity: hiddenKeys.has(p) ? 0.5 : 1.0
+        // type: hiddenLegendKeys.has(p) ? "square" : "circle",
+        fillOpacity: hiddenLegendKeys.has(p) ? 0.5 : 1.0
       }
     };
   });
 };
 
-type State = {
-  hiddenSeries: Set<string>;
+type HiddenItems = {
+  // The series to hide because they have been toggled off.
+  sequences: Set<string>;
+  // The percentiles to hide because they have been toggled off.
+  percentiles: Set<string>;
+
+  // The keys in the legend to hide because they have been toggled off.
+  // Note that while series and percentiles are data on the chart, this is
+  // referring to the legend item, and is how we control the opacity of
+  // differenr elements in the legend.
+  legendKeys: Set<string>;
 };
 
 export default function App() {
-  const [hiddenPercentile, setHiddenPercentile] = useState<State>({
-    hiddenSeries: new Set()
+  const [hiddenItems, setHiddenItems] = useState<HiddenItems>({
+    sequences: new Set(),
+    percentiles: new Set(),
+    legendKeys: new Set()
   });
-  const [hiddenSequence, setHiddenSequence] = useState<State>({
-    hiddenSeries: new Set()
-  });
-  const [hiddenKeys, setHiddenKeys] = useState<Set<string>>(new Set());
   const [series, setSeries] = useState<Array<Line>>([
     {
       sequence: "seq-a",
@@ -138,12 +145,17 @@ export default function App() {
               {
                 target: "data",
                 mutation: (props: any) => {
+                  // Change the opacity of the selected legend key.
+                  const hiddenLegendKeysLocal = hiddenItems.legendKeys;
+                  if (!hiddenLegendKeysLocal.delete(key)) {
+                    hiddenLegendKeysLocal.add(key);
+                  }
+
+                  // Hide either the sequence or the percentile in the plot.
                   let hiddenSeriesLocal =
                     keyName === "sequence"
-                      ? hiddenSequence.hiddenSeries
-                      : hiddenPercentile.hiddenSeries;
-
-                  // We have the percentile and we want to show / hide all lines with that percentile
+                      ? hiddenItems.sequences
+                      : hiddenItems.percentiles;
                   series.forEach((line) => {
                     if (line[keyName] === key) {
                       if (!hiddenSeriesLocal.delete(line.name)) {
@@ -153,21 +165,23 @@ export default function App() {
                       }
                     }
                   });
-                  if (keyName === "sequence") {
-                    setHiddenSequence({
-                      hiddenSeries: new Set(hiddenSeriesLocal)
-                    });
-                  } else {
-                    setHiddenPercentile({
-                      hiddenSeries: new Set(hiddenSeriesLocal)
-                    });
-                  }
-                  const hiddenKeysLocal = hiddenKeys;
-                  if (!hiddenKeysLocal.delete(key)) {
-                    // Was not already hidden => add to set
-                    hiddenKeysLocal.add(key);
-                    setHiddenKeys(hiddenKeysLocal);
-                  }
+
+                  // Change all state at once now that we have modified state data locally.
+                  setHiddenItems({
+                    percentiles:
+                      keyName !== "sequence"
+                        ? new Set(hiddenSeriesLocal)
+                        : hiddenItems.percentiles,
+                    sequences:
+                      keyName === "sequence"
+                        ? new Set(hiddenSeriesLocal)
+                        : hiddenItems.sequences,
+                    legendKeys: new Set(hiddenLegendKeysLocal)
+                  });
+
+                  // NOTE: It is possible to instead modify the legend key opacity here, but doing so
+                  // was not working because by modihing React state we force a re-render which
+                  // wipes out the opacity change. Instead we control the opacity with React state as well.
                   return null;
                 }
               }
@@ -208,8 +222,8 @@ export default function App() {
         <VictoryAxis dependentAxis />
         {series.map((s, idx) => {
           if (
-            hiddenPercentile.hiddenSeries.has(s.name) ||
-            hiddenSequence.hiddenSeries.has(s.name)
+            hiddenItems.percentiles.has(s.name) ||
+            hiddenItems.sequences.has(s.name)
           ) {
             return undefined;
           }
@@ -242,7 +256,7 @@ export default function App() {
         })}
         <VictoryLegend
           name={"legend-percentiles"}
-          data={toVictoryLegend(hiddenKeys, series, "percentile", [
+          data={toVictoryLegend(hiddenItems.legendKeys, series, "percentile", [
             "navy",
             "blue"
           ])}
@@ -252,7 +266,7 @@ export default function App() {
         />
         <VictoryLegend
           name={"legend-sequences"}
-          data={toVictoryLegend(hiddenKeys, series, "sequence", [
+          data={toVictoryLegend(hiddenItems.legendKeys, series, "sequence", [
             "red",
             "green"
           ])}
